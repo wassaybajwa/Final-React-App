9a0aef2e9b285029d50a903c4d9516ea6c64fb50
Wassay Bajwa <71424282+wassaybajwa@users.noreply.github.com>
2021-08-10 23:31:19 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import BasicTitle from "./components/BasicTitle";
+import Buttons, { DefaultButton, HipsterButton } from "./components/Buttons"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BasicTitle>styled components</BasicTitle>
+      <DefaultButton>Click me</DefaultButton>
+      <HipsterButton>Click me</HipsterButton>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/BasicTitle.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const BasicTitle = styled.h1`
+  text-align: center;
+  text-transform: capitalize;
+`
+
+export default BasicTitle
